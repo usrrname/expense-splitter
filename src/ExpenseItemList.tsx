b208093bb79d953f96cf4827672f818bd75dfdb8
Danex2 <khadane.miller@gmail.com>
2019-09-25 19:15:01 -0400

    made redux changes

--- src/ExpenseItemList.tsx
+++ src/ExpenseItemList.tsx
@@ -1,39 +1,36 @@
 import React, { ChangeEvent } from 'react';
-import ExpenseItem from "./ExpenseItem";
+import ExpenseItem from './ExpenseItem';
 import { Item } from './types/types';
 
 type Props = {
-  items: Item[],
-  onChange?: (event: ChangeEvent<HTMLInputElement>) => void,
-  deleteItem: (id: string) => void
-}
+  items: Item[];
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  deleteItem: (id: string) => void;
+};
 
+// adding new items comes up as null for some reason but the state gets updated
+// probably has something to do with the action
 const ExpenseItemList = ({ items, onChange, deleteItem }: Props) => {
-
-  const listItems = items.map((item: Item) =>
+  const listItems = items.map((item: Item) => (
+    // i used some dummy data here so it would render
     <ExpenseItem
-      key={item.id}
-      name={item.name}
-      cost={item.cost}
+      key={'someID'}
+      name={'someItem'}
+      cost={30}
       deleteItem={deleteItem}
       onChange={onChange}
     />
-  )
+  ));
 
   const renderList = () => {
     return (
       <form>
-        <ul className="expense-list">
-          {listItems}
-        </ul>
+        <ul className='expense-list'>{listItems}</ul>
       </form>
-    )
-  }
+    );
+  };
 
-  return (
-    renderList()
-  )
-}
-
+  return renderList();
+};
 
 export default ExpenseItemList;
--- src/store/reducers/ItemListReducer.ts
+++ src/store/reducers/ItemListReducer.ts
@@ -1,37 +1,40 @@
-import { IState, Item, Action } from "../../types/types"
-import { v4 } from "uuid"
-import { Result } from "../actions/types"
-import { Reducer } from "redux";
+import { IState, Item, Action } from '../../types/types';
+import { v4 } from 'uuid';
+import { Result } from '../actions/types';
+import { Reducer } from 'redux';
 import cloneDeep from 'lodash/cloneDeep';
 
 const initialState: IState = {
-	itemCount: 1,
-	items: [{
-		id: v4(),
-		name: "",
-		cost: 0
-	}],
+  itemCount: 1,
+  items: [
+    {
+      id: v4(),
+      name: '',
+      cost: 0
+    }
+  ]
 };
 
 //actions
 export enum ItemActions {
-	ADD_ITEM = "ADD_ITEM",
-	DELETE_ITEM = "DELETE_ITEM"
+  ADD_ITEM = 'ADD_ITEM',
+  DELETE_ITEM = 'DELETE_ITEM'
 }
 
 export const ADD_ITEM = (item: Item): Action => {
-	return {
-		type: ItemActions.ADD_ITEM,
-		payload: item
-	}
-}
+  console.log('test');
+  return {
+    type: ItemActions.ADD_ITEM,
+    payload: item
+  };
+};
 
 export const DELETE_ITEM = (id: string): Action => {
-	return {
-		type: ItemActions.DELETE_ITEM,
-		payload: id
-	}
-}
+  return {
+    type: ItemActions.DELETE_ITEM,
+    payload: id
+  };
+};
 
 // export const GET_TOTAL = (): Action => {
 // 	return {
@@ -42,51 +45,57 @@ export const DELETE_ITEM = (id: string): Action => {
 type ListActions = ReturnType<typeof ADD_ITEM> | ReturnType<typeof DELETE_ITEM>;
 
 //reducer
-export const ItemListReducer: Reducer<IState, Action> = (state = initialState, action: ListActions) => {
-	switch (action.type) {
-		case `${ADD_ITEM}_SUCCESS`:
-			return {
-				...state,
-				items: [...state.items, action.payload]
-			}
-		case `${DELETE_ITEM}_SUCCESS`:
-			return {
-				...state,
-				items: state.items.filter((item, index) => index !== action.index)
-			}
-		default:
-			return state
-	}
-}
+export const ItemListReducer: Reducer<IState, Action> = (
+  state = initialState,
+  action: ListActions
+) => {
+  switch (action.type) {
+    // here's what i changed that made it work, my initial thought was that the name you used for these cases didn't match up
+    // with the action names you were dispatching. the code for updating the state was right
+    case ItemActions.ADD_ITEM:
+      return {
+        ...state,
+        items: [...state.items, action.payload]
+      };
+    case ItemActions.DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter((item, index) => index !== action.index)
+      };
+    default:
+      return state;
+  }
+};
 
 // Action Creators
 
+// here's where the next problem is, getting the new item data into the state
 export const addItem = (): Result<void> => {
-	let newItem: Item = {
-		id: v4(),
-		name: "",
-		cost: 0
-	}
-	return (dispatch, getState) => {
-		let items = getState().ItemList.items
-		const currentCopy = cloneDeep(items);
+  let newItem: Item = {
+    id: v4(),
+    name: '',
+    cost: 0
+  };
+  console.log('test');
+  return (dispatch, getState) => {
+    let items = getState().ItemList.items;
+    const currentCopy = cloneDeep(items);
 
-		items = currentCopy.concat(newItem)
+    items = currentCopy.concat(newItem);
 
-		dispatch({ type: ItemActions.ADD_ITEM, items })
-	}
-}
+    dispatch({ type: ItemActions.ADD_ITEM, items });
+  };
+};
 
 export const deleteItem = (id: string): Result<void> => {
+  return (dispatch, getState) => {
+    let items = cloneDeep(getState().ItemList.items);
 
-	return (dispatch, getState) => {
-		let items = cloneDeep(getState().ItemList.items)
-
-		items.filter(item => item.id !== id)
-		console.log(items);
-		dispatch({ type: ItemActions.DELETE_ITEM, id })
-	}
-}
+    items.filter(item => item.id !== id);
+    console.log(items);
+    dispatch({ type: ItemActions.DELETE_ITEM, id });
+  };
+};
 
 //calculate total
 // export const getTotal = (): Result<void> => {
@@ -100,4 +109,3 @@ export const deleteItem = (id: string): Result<void> => {
 // 		dispatch({ type: "GET_TOTAL", total })
 // 	}
 // }
-
